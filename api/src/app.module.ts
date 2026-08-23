bd98de3681383c285bc64d031df4430f3f4162bc
dani-611 <danifadifarouk6@gmail.com>
2026-08-23 14:05:15 +0300

    removed global unnecessary prettier and eslint files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [AppService],
+      useFactory: (appService: AppService) => ({
+        type: 'postgres',
+        host: appService.getDatabaseHost(),
+        port: appService.getDatabasePort(),
+        username: appService.getDatabaseUsername(),
+        password: appService.getDatabasePassword(),
+        database: appService.getDatabaseName(),
+        autoLoadEntities: true,
+        synchronize: false,
+        logging: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
+  getDatabaseHost(): string | undefined {
+    return this.configService.get<string>('DB_HOST');
+  }
+  getDatabasePort(): number | undefined {
+    return this.configService.get<number>('DB_PORT', 5432);
+  }
+  getDatabaseUsername(): string | undefined {
+    return this.configService.get<string>('DB_USERNAME');
+  }
+  getDatabasePassword(): string | undefined {
+    return this.configService.get<string>('DB_PASSWORD');
+  }
+  getDatabaseName(): string | undefined {
+    return this.configService.get<string>('DB_NAME');
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
